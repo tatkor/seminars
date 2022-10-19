c24a0c12f91ca1bba9ec7eb927e10a9009154adf
tatkor <tatkor@mail.ru>
2022-10-19 12:55:18 +0300

    дз_1-1

--- app_3.py
+++ app_3.py
@@ -4,21 +4,22 @@
 # x=34; y=-30 -> 4
 # x=2; y=4-> 1
 # x=-34; y=-30 -> 3
-
-x, y = int(input('Введите координаты x= ')), int(input('Введите координаты y= '))
-s = (x, y)
-#print(s[0], type(s))
-if s[0] > 0 and s[1] > 0:
-    print('1')
-elif s[0] > 0 and s[1] < 0:
-    print('4')
-elif s[0] < 0 and s[1] < 0:
-    print('3')
-elif s[0] < 0 and s[1] > 0:
-    print('2')
-elif s[0] == 0:
-    print('ось Y')
-elif s[1] == 0:
-    print('ось X')
-else:
-    print('Начало координат')
+try:
+    x, y = int(input('Введите координаты x= ')), int(input('Введите координаты y= '))
+    s = (x, y)
+    if s[0] > 0 and s[1] > 0:
+        print('1')
+    elif s[0] > 0 and s[1] < 0:
+        print('4')
+    elif s[0] < 0 and s[1] < 0:
+        print('3')
+    elif s[0] < 0 and s[1] > 0:
+        print('2')
+    elif s[0] == 0:
+        print('ось Y')
+    elif s[1] == 0:
+        print('ось X')
+    else:
+        print('Начало координат')
+except:
+    print('Введите целое число')
